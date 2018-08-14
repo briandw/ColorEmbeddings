ed62f048b3b181fb48d451799cbea0120b8777a0
Brian <git@chromaticgray.com>
2018-08-13 21:04:58 -0700

    fixed the cleanup code

--- graph_utils.py
+++ graph_utils.py
@@ -29,6 +29,7 @@ def fig2data ( fig ):
  
     # Get the RGBA buffer from the figure
     w,h = fig.canvas.get_width_height()
+    fig.canvas.draw()
     buf = np.frombuffer ( fig.canvas.tostring_argb(), dtype=np.uint8 )
     buf.shape = ( w, h,4 )
  
@@ -48,19 +49,27 @@ def fig2img ( fig ):
     return PIL.Image.frombytes( "RGBA", ( w ,h ), buf.tostring( ) )
 
 def pltimg(x, y, z=None, filter_len=10):
-    figure = Figure()
-    canvas = FigureCanvas(figure)
-    ax = figure.add_subplot(1, 1, 1)
-    ax.set_xlabel('time')
-    ax.set_ylabel('loss')
-    ax.plot(x, y, 'b')
+    fig, ax1 = plt.subplots()
+    color = 'blue'
+    ax1.set_xlabel('time')
+    ax1.set_ylabel('loss', color=color)
+    ax1.plot(x, y, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
     if len(y) > filter_len:
         y = scipy.signal.savgol_filter(y, filter_len-1, 1)
-        ax.plot(x, y, 'r')
+        ax1.plot(x, y, 'r')
+
     if z != None:
-        ax.plot(x, z, 'g')
-        
-    return fig2img(figure)
+        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+        color = 'green'
+        ax2.set_ylabel('learning rate', color=color)  # we already handled the x-label with ax1
+        ax2.plot(x, z, color=color)
+        ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    image = fig2img(fig)
+    plt.close(fig)
+    return image
 
 
 plot_display_id = "ploy_display_id"
